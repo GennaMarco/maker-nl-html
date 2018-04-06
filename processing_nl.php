e46e74259e4f81188e3d49506ed809e0e0d9bba1
Marco <marco.degennaro@edu.itspiemonte.com>
2018-04-06 12:53:40 +0200

    Implemented reader's class for files doc/docx

--- processing_nl.php
+++ processing_nl.php
@@ -6,22 +6,21 @@
  * Time: 12:18
  */
 
-//percorso della cartella dove mettere i file caricati dagli utenti
-$uploaddir = 'files/';
+require_once('classes/DocConversion.php');
 
-//Recupero il percorso temporaneo del file
-$userfile_tmp = $_FILES['FileToUpload']['tmp_name'];
+$upload_dir = 'files';
+$file_tmp = $_FILES['FileToUpload']['tmp_name'];
+$file_name = $_FILES['FileToUpload']['name'];
 
-//recupero il nome originale del file caricato
-$userfile_name = $_FILES['FileToUpload']['name'];
-
-//copio il file dalla sua posizione temporanea alla mia cartella upload
-if (move_uploaded_file($userfile_tmp, $uploaddir . $userfile_name))
+if (move_uploaded_file($file_tmp, $upload_dir . '/' . $file_name))
 {
-    //Se l'operazione è andata a buon fine...
-    echo 'File inviato con successo.';
-}else
-{
-    //Se l'operazione è fallta...
-    echo 'Upload NON valido!';
+    echo 'File uploaded!<br>';
 }
+else
+{
+    echo 'File not uploaded!<br>';
+}
+
+$docObj = new DocConversion($upload_dir . '/' . $file_name);
+
+echo $docText = $docObj->convertToText();
