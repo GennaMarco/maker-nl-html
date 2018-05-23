1f11b0c7f78be2ffc6b7ef28732ef059f518ee64
Marco <marco.degennaro@edu.itspiemonte.com>
2018-05-23 15:34:31 +0200

    Added zip archive of nl

--- processing_nl.php
+++ processing_nl.php
@@ -236,3 +236,31 @@ $marginEnd = $doc->createCDATASection
 $parent->appendChild($marginEnd);
 
 $doc->saveHTMLFile($pathToNewsletter.'/'.$newsletterDir.'.html');
+
+// Get real path for our folder
+$rootPath = realpath($pathToNewsletter);
+
+// Initialize archive object
+$zip = new ZipArchive();
+$zip->open(DOWNLOAD_DIR."/$newsletterDir.zip", ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+// Create recursive directory iterator
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootPath), RecursiveIteratorIterator::LEAVES_ONLY);
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+// Zip archive will be created only after closing object
+$zip->close();
